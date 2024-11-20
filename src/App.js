8e89b9bd3ceab7f3ff97502a5ed1318234e7ca5d
samir jadhav <samirzjadhav@gmail.com>
2024-11-20 12:50:07 +0530

    fix: change the outlet section on app section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
-      <Outlet />
+      <Header />
+      <div className="pt-16">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
 
 export default App;
+ 
