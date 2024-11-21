22b97a70909f7ac6f8abb87fdc24b53bb29ab076
samir jadhav <samirzjadhav@gmail.com>
2024-11-21 14:02:50 +0530

    feat: added link on header section

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import MobileNavigation from "./components/MobileNavigation";
 
 const App = () => {
   return (
-    <div>
+    <main className="pb-14 lg:pb-0">
       <Header />
       <div className="pt-16">
         <Outlet />
       </div>
       <Footer />
-    </div>
+      <MobileNavigation />
+    </main>
   );
 };
 
 export default App;
- 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,48 @@
 import React, { useEffect, useState } from "react";
 import logo from "../assets/logo.png";
-import { NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import userIcon from "../assets/user.png";
 import { IoSearchOutline } from "react-icons/io5";
+import { MdHomeFilled } from "react-icons/md";
+import { PiTelevisionFill } from "react-icons/pi";
+import { BiSolidMoviePlay } from "react-icons/bi";
+
+export const Navigation = [
+  {
+    label: "Tv Shows",
+    href: "tv",
+    icon: <PiTelevisionFill />,
+  },
+  {
+    label: "Movies",
+    href: "movies",
+    icon: <BiSolidMoviePlay />,
+  },
+];
+
+export const MobileNavigation = [
+  {
+    label: "Home",
+    href: "/",
+    icon: <MdHomeFilled />,
+  },
+];
 
 const Header = () => {
-  const Navigation = [
-    {
-      label: "Tv Shows",
-      href: "tv",
-    },
-    {
-      label: "Movies",
-      href: "movies",
-    },
-  ];
-
   const [searchInput, setSearchInput] = useState("");
   const navigate = useNavigate();
   useEffect(() => {
     navigate(`/search?q=${searchInput}`);
   }, [searchInput]);
 
+  const handleSubmit = (e) => [e.preventDefault()];
+
   return (
     <header className="fixed top-0 w-full h-16 bg-neutral-600 bg-opacity-75">
       <div className="container mx-auto px-3 flex items-center h-full">
-        <div className="">
+        <Link to={"/"}>
           <img src={logo} alt="" width={120} />
-        </div>
+        </Link>
         <nav className="hidden lg:flex items-center gap-2 ml-5">
           {Navigation.map((item, index) => {
             return (
@@ -46,11 +61,15 @@ const Header = () => {
           })}
         </nav>
         <div className="ml-auto flex items-center gap-5 ">
-          <form action="" className="flex items-center gap-2">
+          <form
+            action=""
+            className="flex items-center gap-2"
+            onSubmit={handleSubmit}
+          >
             <input
               type="text"
-              placeholder="Search Here ..."
-              className="bg-transparent px-4 py-1 "
+              placeholder="Search Here..."
+              className="bg-transparent px-4 py-1 hidden lg:block"
               onChange={(e) => setSearchInput(e.target.value)}
               value={searchInput}
             />
