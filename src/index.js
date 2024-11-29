ff85c735ed2e1da5050bff974aa316fde751b179
samir jadhav <samirzjadhav@gmail.com>
2024-11-29 13:19:19 +0200

    feat: finaly using redux on the project

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/route";
 import axios from "axios";
+import store from "./store/store";
+import { Provider } from "react-redux";
 
 // Setup  axios
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
@@ -14,9 +16,12 @@ axios.defaults.headers.common[
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router}>
       <App />
     </RouterProvider>
-  </React.StrictMode>
+  </Provider>
+
+  // </React.StrictMode>
 );
