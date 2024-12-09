2874a701785dbcf3ecb18c1d3933e4c85f727725
samir jadhav <samirzjadhav@gmail.com>
2024-12-09 20:46:31 +0530

    feat: fetch new data from api and get title and overview on it

--- src/components/BannerHome.js
+++ src/components/BannerHome.js
@@ -13,7 +13,7 @@ const BannerHome = () => {
 
   return (
     <section className="w-full h-full">
-      <div className="flex min-h-full max-h-[95vh]">
+      <div className="flex min-h-full max-h-[95vh] overflow-hidden">
         {bannerData.map((data, index) => {
           return (
             <div
@@ -27,10 +27,26 @@ const BannerHome = () => {
                   alt=""
                   className="h-full w-full object-cover"
                 />
-                <h3>{data.title}</h3>
-                <p>{data.description}</p>
               </div>
               <div className="absolute top-0 w-full h-full bg-gradient-to-t from-neutral-900 to-transparent"></div>
+              <div className="container mx-auto ">
+                <div className="w-full absolute bottom-0 max-w-md px-3">
+                  <h2 className="font-bold text-2xl lg:text-4xl text-white drop-shadow-2xl">
+                    {data.title}
+                  </h2>
+                  <p className="text-ellipsis line-clamp-3 my-2">
+                    {data.overview}
+                  </p>
+                  <div className="flex items-center gap-4">
+                    <p>Rating: {Number(data.vote_average).toFixed(1)}+ </p>
+                    <span>|</span>
+                    <p>View: {Number(data.popularity).toFixed(0)}</p>
+                  </div>
+                  <button className="bg-white px-4 py-2 text-black font-bold rounded mt-4 hover:scale-105 hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all">
+                    Play Now
+                  </button>
+                </div>
+              </div>
             </div>
           );
         })}
