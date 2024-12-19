6210ad7bd378a2946986b3914d0bc8a8c990ad7b
samir jadhav <samirzjadhav@gmail.com>
2024-12-19 20:53:02 +0530

    feat: added new footer and chnage a mobilenavigation section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="text-center bg-neutral-600 bg-opacity-35 text-neutral-400 py-4 ">
+      <div className="flex items-center justify-center gap-4">
+        <Link to="/">About</Link>
+        <Link to="/">Contact</Link>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
